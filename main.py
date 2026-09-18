ef53e1f6b2246d79ba1e2748ef7df2e97ac135f0
haovhd <haovhd@gmail.com>
2026-09-18 14:45:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 
 # 配置区域
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6666666-6666-6666-6666-66666666666",
+    "https://host2play.gratis/server/renew?i=75db5182-5b3c-46eb-9435-525d893100ab",
     # 添加更多链接
 ]
 
